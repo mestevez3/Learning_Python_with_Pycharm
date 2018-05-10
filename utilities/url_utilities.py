603d9e1a60e002b873321b5666e9130ac765ac79
mestevez3 <mestevez3@gatech.edu>
2018-05-09 21:40:54 -0700

    Initial Commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    #TODO: ad code needed to read text
+    pass
+
+def load_page(url:str):
+    #TODO: add code that reads the url
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
