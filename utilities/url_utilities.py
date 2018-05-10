9e48f74b44f00d67678a5637182a400d6a59fa38
mestevez3 <mestevez3@gatech.edu>
2018-05-10 08:31:09 -0700

    Fixed formatting issues.

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,11 +1,13 @@
 def load_urls_from_file(file_path: str):
-    #TODO: ad code needed to read text
+    # TODO: ad code needed to read text
     pass
 
-def load_page(url:str):
-    #TODO: add code that reads the url
+
+def load_page(url: str):
+    # TODO: add code that reads the url
     pass
+
 
 def scrape_page(page_contents: str):
-    #TODO: analyze the text
+    # TODO: analyze the text
     pass
